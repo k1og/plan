e8d1d566be2480a40a32099cc3dfa3841e3deefd
Alexey <headofhell@gmail.com>
2019-07-02 23:44:45 +0400

    Project created

---
+++ ItUniver_PlanManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItUniver_PlanManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
